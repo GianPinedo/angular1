61fff31d605560cab1b9fdd8f1dcda955940c6bd
gp <gppproyect@gmail.com>
2024-01-21 21:10:03 -0500

    domingo

--- src/app/pages/detail/detail.component.ts
+++ src/app/pages/detail/detail.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
-
-
+import { environment } from 'src/app/environment';
 @Component({
   selector: 'app-detail',
   templateUrl: './detail.component.html',
@@ -12,21 +11,17 @@ import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 export class DetailComponent implements OnInit {
   mealId: any;
   mealDetails: any;
-  //safeYoutubeUrl: SafeResourceUrl;
-
   constructor(
     private route: ActivatedRoute,
     private http: HttpClient,
     private sanitizer: DomSanitizer,
-    //private safeYoutubeUrl: SafeResourceUrl
   ) { }
 
   ngOnInit(): void {
-    // Obtener el ID del platillo desde la URL
+    const apiBaseUrl = environment.apiBaseUrl;
     this.route.params.subscribe(params => {
       this.mealId = params['id'];
-      // Realizar una solicitud para obtener los detalles del platillo por su ID
-      this.http.get('https://www.themealdb.com/api/json/v1/1/lookup.php?i=' + this.mealId)
+      this.http.get(`${apiBaseUrl}lookup.php?i=` + this.mealId)
         .subscribe((data: any) => {
           this.mealDetails = data.meals[0];
         });
