9966660c182c415ec633bebc56edbfddb92ff36c
Soporte TI <soporte@pronatel.gob.pe>
2024-01-22 10:55:57 -0500

    test

---
+++ karma.conf.js
@@ -0,0 +1,27 @@
+module.exports = function (config) {
+    config.set({
+      frameworks: ['jasmine', '@angular-devkit/build-angular'],
+      plugins: [
+        require('karma-jasmine'),
+        require('karma-chrome-launcher'),
+        require('karma-coverage-istanbul-reporter'),
+        require('@angular-devkit/build-angular/plugins/karma')
+      ],
+      client: {
+        clearContext: false // deja Jasmine manejar la toma de decisiones al limpiar el contexto en cada prueba
+      },
+      coverageIstanbulReporter: {
+        dir: require('path').join(__dirname, './coverage/angular1'),
+        reports: ['html', 'lcovonly', 'text-summary'],
+        fixWebpackSourcePaths: true
+      },
+      reporters: ['progress', 'kjhtml'],
+      port: 9876,
+      colors: true,
+      logLevel: config.LOG_INFO,
+      autoWatch: true,
+      browsers: ['Chrome'],
+      singleRun: false
+    });
+  };
+  
--- src/app/pages/inicio/inicio.component.spec.ts
+++ src/app/pages/inicio/inicio.component.spec.ts
@@ -1,16 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { InicioComponent } from './inicio.component';
+import { IndexComponent } from './inicio.component';
 
 describe('InicioComponent', () => {
-  let component: InicioComponent;
-  let fixture: ComponentFixture<InicioComponent>;
+  let component: IndexComponent;
+  let fixture: ComponentFixture<IndexComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [InicioComponent]
+      declarations: [IndexComponent]
     });
-    fixture = TestBed.createComponent(InicioComponent);
+    fixture = TestBed.createComponent(IndexComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
